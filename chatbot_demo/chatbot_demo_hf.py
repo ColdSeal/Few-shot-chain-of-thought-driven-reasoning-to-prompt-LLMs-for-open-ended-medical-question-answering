b44fce8858a726660e2fee922409f5ea1fa4f5ff
ColdSeal <saeel@iitb-dgx4.iitb.ac.in>
2024-10-03 04:51:50 +0530

    Final commit

--- chatbot_demo/chatbot_demo_hf.py
+++ chatbot_demo/chatbot_demo_hf.py
@@ -13,10 +13,6 @@ from huggingface_hub import login
 hf_token = "<Enter your token>"
 login(token = hf_token)
 
-api_key = "pak-zLIXfwz3UmF2EIOrXr-T4YNw8s3A0MUCZ-5pmFezfFY"
-api_url = "https://bam-api.res.ibm.com/v1"
-creds = Credentials(api_key,api_url)
-
 device = torch.device("cuda")
 
 if 'submit' not in st.session_state:
@@ -60,8 +56,6 @@ def option_exists(new_op,old_ops):
     new_op = new_op.strip(". ").lower()
     for old_op in old_ops:
         old_op = old_op.strip(". ").lower()
-        # print(f"new:{new_op}")
-        # print(f"old:{old_op}")
         if (new_op in old_op or old_op in new_op):
             return(1)
     return(0)
@@ -92,53 +86,33 @@ def create_options(instring, num_unique_ops = 4, options_generate_limit = 15):
             op_reasoning, op_text  = text_output.split(reasoning_delimiter)
             op_reasoning, op_text = op_reasoning.strip(), op_text.strip().split("\n")[0].strip()
 
-            # print("Option Created :",op_text)
             st.write("Option Created :",op_text)
 
             if not option_exists(op_text,unique_options):
-                # print("New Option :", op_text)
+
                 st.write("New Option Created : Accepted")
                 unique_options.append(op_text)
                 op_to_reason[op_text] = op_reasoning
             else:
-                # print("Option already exists.. Discarding..")
                 st.write("Option already exists.. Discarding..")
             
         else:
             op_text = "<parsing error>"
-            # print(op_text)
             st.write(op_text)
 
         all_options.append(op_text)
-        # print("Existing Options :", unique_options, "\n")
-        # st.write("Existing Options :", unique_options, "\n")
 
-    # print("Final Options :", unique_options)
     st.write("Final Options :", unique_options)
 
     return [unique_options,op_to_reason]
 
-# st.write("""
-# # DEMO for running multiple prompt interfaces
-# """)
-
 st.title("Choose the algorithm you want to run :")
-# option_ind = int(input("choose an algorithm to run"))
 algorithms = ["MedCodex - Greedy", "Codex - Greedy", "MedCodex + Codex (F+B)", "MedCodex + Verifier (F + RM)"]
-# algo = algorithms[option_ind]
-# Drop-down menu with four options
 algo = st.selectbox("Choose an option:", algorithms)
 
-# Display the selected optio
-# st.write("You selected:", option)
-# print("You selected:", algo)
 st.write("You selected:", algo)
 
-# user_input = st.text_input("Enter input prompt")
 user_input = st.text_area("Enter input prompt", value="", height=50, max_chars=None)
-# user_input = input("User input here : ")
-# print("The input is :\n",user_input)
-# st.write("The input is :\n",user_input)
 
 submit = st.button("Submit input")
 
@@ -161,11 +135,7 @@ if algo == "MedCodex - Greedy" or algo == "Codex - Greedy":
 
         instring = f'''{few_shot_cot}\n\nQ: {user_input}\nA: Let's think step-by-step.'''
 
-        # submit = True
-
     if submit:
-
-        # st.write("The input is :\n",user_input)
 
         inputs = tokenizer(instring, return_tensors="pt")
         inputs = inputs.to(device)
@@ -181,7 +151,6 @@ if algo == "MedCodex - Greedy" or algo == "Codex - Greedy":
             greedy_ans = "<parsing error>"
 
         output = greedy_output
-        # output = "F output"
 
 elif algo == "MedCodex + Codex (F+B)" or algo == "MedCodex + Verifier (F + RM)":
 
@@ -198,9 +167,7 @@ elif algo == "MedCodex + Codex (F+B)" or algo == "MedCodex + Verifier (F + RM)":
         uniq_options, op_to_reason = create_options(f_instring)
         
         if (len(uniq_options) < 4):
-            # output = "Not enough options generated!"
             st.write("Not enough Options")
-            # print("Not enough Options")
 
         else:
             if algo == "MedCodex + Codex (F+B)":
@@ -223,11 +190,7 @@ elif algo == "MedCodex + Codex (F+B)" or algo == "MedCodex + Verifier (F + RM)":
 
                 if b_output.strip() == "":
                     st.write("output empty")
-                    # print("output empty")
                     b_output = "<empty>"
-
-                # if str(elem["a"]).strip() == "":
-                #     elem["a"] = "<empty>"
 
                 b_answer = re.findall(r"\([A-D]\)",b_output)
                 if len(b_answer):
@@ -239,49 +202,6 @@ elif algo == "MedCodex + Codex (F+B)" or algo == "MedCodex + Verifier (F + RM)":
                     b_answer ="<parsing error>" 
                     option = 0
                     output = "<parsing error>"
-
-
-                # # backward_ques = input("Input backward Question : ")
-                # backward_ques = st.text_area("Enter backward Question", value="", height=50, max_chars=None)
-
-                # submit_backward = st.button("Submit backward input")
-
-                # if submit_backward or st.session_state.submit_backward:
-                    
-                #     st.session_state.submit_backward = True
-
-                #     b_instring = f"{backward_prompt}\n\nQ: {backward_ques}\n{options_text}A: Let's think step-by-step."
-
-                #     b_inputs = tokenizer(b_instring, return_tensors="pt")
-                #     b_inputs = b_inputs.to(device)
-
-                #     # b_out = model.generate([b_instring])[0]
-                #     # b_output = b_out.generated_text.strip()
-
-                #     b_output = model.generate(b_inputs.input_ids, max_new_tokens = 1024, repetition_penalty = 1.1)
-                #     b_output = tokenizer.batch_decode(b_output, skip_special_tokens=True)[0]
-                #     b_output = b_output.split(b_instring)[-1]
-                #     b_output = b_output.strip()
-
-                #     if b_output.strip() == "":
-                #         st.write("output empty")
-                #         # print("output empty")
-                #         b_output = "<empty>"
-
-                #     # if str(elem["a"]).strip() == "":
-                #     #     elem["a"] = "<empty>"
-
-                #     b_answer = re.findall(r"\([A-D]\)",b_output)
-                #     if len(b_answer):
-                #         b_answer = b_answer[0]
-                #         option = ord(b_answer[1]) - ord('A') + 1
-                #         b_answer_idx = ord(b_answer[1]) - ord('A')
-                #         output = b_answer + " " + op_to_reason[uniq_options[b_answer_idx]] + "\nAnswer: " + uniq_options[b_answer_idx]
-                #     else:
-                #         b_answer ="<parsing error>" 
-                #         option = 0
-                #         output = "<parsing error>"
-
 
             elif algo == "MedCodex + Verifier (F + RM)":
 
@@ -297,30 +217,18 @@ elif algo == "MedCodex + Codex (F+B)" or algo == "MedCodex + Verifier (F + RM)":
                 with torch.no_grad():
                     outputs = reward_model(**inputs)
 
-                # logit = outputs.logits.item()
-                # logits.append(logit)
-
                 logits = outputs.logits
                 logits = logits.reshape(-1)
                 logits = logits.tolist()
-
-                # print('logit', chr(ord('A') + k-1), f"model : {logit:.3f}")
 
                 answer_idx = np.argmax(logits)
                 answer = f"({chr(answer_idx + ord('A'))})"
 
                 output = "Option " + answer + "\n" + op_to_reason[uniq_options[answer_idx]] + "\nAnswer: " + uniq_options[answer_idx]
 
-# st.write("Your desired output is as follows :\n", output)
 st.write("""
     <div style='border: 1px solid black; padding: 10px;'>
         <h3>Your desired output is as follows:</h3>
         <blockquote>{}</blockquote>
     </div>
     """.format(output), unsafe_allow_html=True)
-# print("Your desired output is as follows :\n", output)
-
-# clear = st.button("Clear")
-# if clear:
-#     st.session_state.submit_backward = False
-#     st.session_state.submit = False
